c0af873ce9d07b1a9c0a744e5bfbc9017534e871
mlaplv <mlaplv@gmail.com>
2026-06-23 17:54:41 +0700

    update sge kng

--- frontend/src/lib/components/storefront/funnel/MobileFunnelManager.svelte
+++ frontend/src/lib/components/storefront/funnel/MobileFunnelManager.svelte
@@ -94,6 +94,7 @@
     ])).filter(Boolean),
     ratingValue: data?.reviewStats?.average_rating || 5,
     reviewCount: data?.reviewStats?.total_count || 1,
+    knowledge_graph: product?.metadata?.knowledge_graph
   }}
   jsonLdScripts={[
     seoMeta?.json_ld_string,
